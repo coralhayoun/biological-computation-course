2c0f9b01c7360b24ebcbecc8315e3c74e5236837
Coral Hayoun <koralhayoun@gmail.com>
2023-12-29 21:25:40 +0200

    Added req

--- ex-11/app.py
+++ ex-11/app.py
@@ -4,7 +4,6 @@ from models.automaton.neighborhood.neighborhood import NeighborhoodName, Neighbo
 from constants.transition_rules import transition_rules
 
 from models.ui.canvas import Canvas
-from models.ui.graph_displater import GraphDisplayer
 
 # init celular automaton
 cells_matrix_factory = CellsMatrixFactory()
@@ -18,7 +17,3 @@ automation = CellularAutomaton(cells_matrix, Neighborhood(NeighborhoodName.NEUMA
 # init canvas (ui representation)
 canvas = Canvas(automation)
 canvas.init_canvas()
-
-# display graph over the data
-graph_displayer = GraphDisplayer(canvas.daily_temperature_avg, canvas.daily_air_pollution_avg)
-graph_displayer.display_linear_graph()
